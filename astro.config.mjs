1a1290a8c6ccfe5654c50a8f307756cf4f3595fe
Yuval728 <yuvalmehta.728@gmail.com>
2025-05-17 18:24:25 +0530

    Add sitemap and robots.txt files, update dependencies
    - Created `robots.txt` in `dist` and `public` directories to allow all user agents.
    - Added `sitemap-0.xml` and `sitemap-index.xml` for better SEO.
    - Updated `package-lock.json` and `package.json` to include `@astrojs/sitemap` dependency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 // import github from '@astrojs/github';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   // adapter: github(),
   site: 'https://yuval728.github.io',
   // base: '/',
